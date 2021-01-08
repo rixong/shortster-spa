5c3e13cc663f6107aa13d7e8acda0a43c3e70ea8
rixong <rixong@gmail.com>
2021-01-08 15:24:20 -0600

    set up initial form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 
 function App() {
   return (
-    
+
     <main className="App">
       <h1>Shortster</h1>
       <div>
         <form>
           <label htmlFor="longURL">Enter original URL</label>
-          <input type="text" name="longURL" id="longURL">
-          </input>
+          <input type="text" name="longURL" id="longURL"></input>
+          <label htmlFor="shortURL">Enter custom URL (optional)</label>
+          <input type="text" name="shortURL" id="shortURL"></input>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </main>
-    
+
   );
 }
 
