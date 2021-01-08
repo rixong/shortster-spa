d99ea41ef845300b4c3ad2ebf85b78760c22da57
rixong <rixong@gmail.com>
2021-01-08 08:22:04 -0600

    intitial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <main className="App">
+      <h1>Shortster</h1>
+      <div>
+        <form>
+          <label htmlFor="longURL">Enter original URL</label>
+          <input type="text" name="longURL" id="longURL">
+          </input>
+        </form>
+      </div>
+    </main>
+    
   );
 }
 
