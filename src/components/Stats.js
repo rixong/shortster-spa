b3b2deca41ee23e94784bfd714f059c863c55324
rixong <rixong@gmail.com>
2021-01-11 09:51:30 -0600

    install luxon, format datetime

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,11 +1,20 @@
 import React from 'react';
+const { DateTime } = require('luxon');
+
 
 const Stats = ({URLObj}) => {
+
+  const formatDate = (dateObj) => {
+    return DateTime.fromISO(dateObj).toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)
+  }
+
+  const dt = DateTime.fromISO(URLObj.createdAt).toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS)
+  console.log(dt)
   return (
     <div>
       <h3>Stats for short URL: {URLObj.shortURL}</h3>
-      <p>Registered on: <em>{URLObj.createdAt}</em></p>
-      <p>Last accessed: <em>{URLObj.lastAccessed}</em></p>
+      <p>Registered on: <em>{formatDate(URLObj.createdAt)}</em></p>
+      <p>Last accessed: <em>{formatDate(URLObj.lastAccessed)}</em></p>
       <p>Total accesses: <em>{URLObj.numberOfAccesses}</em></p>
     </div>
   )
